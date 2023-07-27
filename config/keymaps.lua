a50359d04e331eac66664579ec0184363806016b
Noam Muallem <noam.muallem@pointfive.co>
2023-07-27 10:05:19 +0300

    amazing keymap for window resize

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -71,3 +71,48 @@ vim.keymap.set("n", "<leader>N", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 
 -- zenMode
 vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "ZenMode" })
+
+-- smart resizing splits
+local change_width = function(d)
+  local v = vim.api
+
+  -- Lua version of a ternery operator
+  d = d and d or "left"
+
+  local lr = d == "left" or d == "right"
+  -- 5 for left right, 3 for up down
+  local amt = lr and 5 or 3
+
+  local pos = v.nvim_win_get_position(0)
+  local w = v.nvim_win_get_width(0)
+  local h = v.nvim_win_get_height(0)
+
+  if lr then
+    amt = pos[2] == 0 and -amt or amt
+  else
+    amt = pos[1] == 0 and -amt or amt
+  end
+
+  w = (d == "left") and (w + amt) or (w - amt)
+  h = (d == "up") and (h + amt) or (h - amt)
+
+  if lr then
+    v.nvim_win_set_width(0, w)
+  else
+    v.nvim_win_set_height(0, h)
+  end
+end
+
+vim.keymap.set({ "n" }, "<S-left>", function()
+  change_width("left")
+end)
+vim.keymap.set({ "n" }, "<S-right>", function()
+  change_width("right")
+end)
+
+vim.keymap.set({ "n" }, "<S-up>", function()
+  change_width("up")
+end)
+vim.keymap.set({ "n" }, "<S-down>", function()
+  change_width("down")
+end)
