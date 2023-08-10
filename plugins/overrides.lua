6b7a15759b5d2409c7341c6171f6e6a52baddb53
Noam Muallem <noam.muallem@pointfive.co>
2023-08-10 10:59:52 +0300

    added git blame

--- plugins/overrides.lua
+++ plugins/overrides.lua
@@ -123,7 +123,7 @@ return {
           ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<S-Tab>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<S-CR>"] = cmp.mapping.confirm({
@@ -228,4 +228,5 @@ return {
   { "Shatur/neovim-ayu" },
   { "EdenEast/nightfox.nvim" },
   { "navarasu/onedark.nvim" },
+  { "f-person/git-blame.nvim" },
 }
